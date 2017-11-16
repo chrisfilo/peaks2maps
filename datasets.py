d75bd2ce404a23d6b5681a60259d8c6d4bedf655
Chris Gorgolewski <krzysztof.gorgolewski@gmail.com>
2017-11-15 18:38:19 -0800

    refactor datasets

--- datasets.py
+++ datasets.py
@@ -7,77 +7,29 @@ import nibabel as nb
 import numpy as np
 from scipy.ndimage.interpolation import zoom
 from nilearn import image
-
-# class Peaks2MapsDataset:
-#
-#     def __init__(self, conform_shape=(64, 64, 64)):
-#         self.conform_shape = conform_shape
-#
-#     def fill_feed_dict(data_set, images_pl, labels_pl):
-#         """Fills the feed_dict for training the given step.
-#         A feed_dict takes the form of:
-#         feed_dict = {
-#             <placeholder>: <tensor of values to be passed for placeholder>,
-#             ....
-#         }
-#         Args:
-#           data_set: The set of images and labels, from input_data.read_data_sets()
-#           images_pl: The images placeholder, from placeholder_inputs().
-#           labels_pl: The labels placeholder, from placeholder_inputs().
-#         Returns:
-#           feed_dict: The feed dictionary mapping from placeholders to values.
-#         """
-#         # Create the feed_dict for the placeholders filled with the next
-#         # `batch size` examples.
-#         images_feed, labels_feed = data_set.next_batch(FLAGS.batch_size,
-#                                                        FLAGS.fake_data)
-#         feed_dict = {
-#             images_pl: images_feed,
-#             labels_pl: labels_feed,
-#         }
-#         return feed_dict
+from nilearn.plotting import plot_glass_brain
+import io
 
 
-
-def get_data(batch_size, nthreads=8):
-    training_dataset, target_shape = _get_data(nthreads, batch_size,
-                                               "D:/data/hcp_statmaps/train", 100,
-                                               'D:/drive/workspace/peaks2maps/cache_train',
-                                               True)
-    validation_dataset, target_shape = _get_data(nthreads, 1,
-                                                 "D:/data/hcp_statmaps/val", 1,
-                                                 'D:/drive/workspace/peaks2maps/cache_val',
-                                                 False)
-    handle = tf.placeholder(tf.string, shape=[])
-    iterator = tf.data.Iterator.from_string_handle(
-        handle, training_dataset.output_types, training_dataset.output_shapes)
-    input, target = iterator.get_next()
-
-    # You can use feedable iterators with a variety of different kinds of iterator
-    # (such as one-shot and initializable iterators).
-    training_iterator = training_dataset.make_one_shot_iterator()
-    validation_iterator = validation_dataset.make_initializable_iterator()
-
-    return input, target, target_shape, handle, training_iterator, validation_iterator
+def _get_resize_arg(target_resolution_mm):
+    mni_shape_mm = np.array([148.0, 184.0, 156.0])
+    target_shape = np.ceil(mni_shape_mm / target_resolution_mm).astype(
+        np.int32)
+    target_affine = np.array([[4., 0., 0., -75.],
+                              [0., 4., 0., -105.],
+                              [0., 0., 4., -70.],
+                              [0., 0., 0., 1.]])
+    target_affine[0, 0] = target_affine[1, 1] = target_affine[
+        2, 2] = target_resolution_mm
+    return target_affine, list(target_shape)
 
 
-def _get_data(nthreads, batch_size, src_folder, n_epochs, cache, shuffle):
+def _get_data(nthreads, batch_size, src_folder, n_epochs, cache, shuffle,
+              target_resolution):
     filenames = tf.constant(glob(os.path.join(src_folder, "*.nii.gz")))
     dataset = tf.data.Dataset.from_tensor_slices((filenames,))
 
-    def _get_resize_arg(target_resolution_mm):
-        mni_shape_mm = np.array([148.0, 184.0, 156.0])
-        target_shape = np.ceil(mni_shape_mm / target_resolution_mm).astype(
-            np.int32)
-        target_affine = np.array([[4., 0., 0., -75.],
-                                  [0., 4., 0., -105.],
-                                  [0., 0., 4., -70.],
-                                  [0., 0., 0., 1.]])
-        target_affine[0, 0] = target_affine[1, 1] = target_affine[
-            2, 2] = target_resolution_mm
-        return target_affine, list(target_shape)
-
-    target_affine, target_shape = _get_resize_arg(4.0)
+    target_affine, target_shape = _get_resize_arg(target_resolution)
 
     def _read_py_function(filename, target_affine, target_shape):
         nii = image.resample_img(
@@ -89,34 +41,19 @@ def _get_data(nthreads, batch_size, src_folder, n_epochs, cache, shuffle):
 
     target_affine_tf = tf.constant(target_affine)
     target_shape_tf = tf.constant(target_shape)
-    dataset = dataset.map(lambda filename: tuple(tf.py_func(_read_py_function,
-                                                            [filename,
-                                                             target_affine_tf,
-                                                             target_shape_tf],
-                                                            [tf.float32])),
-                          num_parallel_calls=nthreads)
+    dataset = dataset.map(
+        lambda filename: tuple(tf.py_func(_read_py_function,
+                                          [filename,
+                                           target_affine_tf,
+                                           target_shape_tf],
+                                          [tf.float32])),
+        num_parallel_calls=nthreads)
 
     def _resize(data):
         return tf.reshape(data, target_shape)
 
     dataset = dataset.map(_resize)
-    # def _smooth_py_function(data):
-    #     target_shape = np.array([64, 64, 64])
-    #     zoom_factors = target_shape/data.shape
-    #     return zoom(data, zoom_factors, order=0).astype(np.float32)
-    #
-    # dataset = dataset.map(lambda data: tuple(tf.py_func(_resize_py_function,
-    #                                                     [data],
-    #                                                     [tf.float32])))
-    # def _resize_py_function(data, target_affine, target_shape):
-    #     zoom_factors = target_shape/data.shape
-    #     return zoom(data, zoom_factors, order=0).astype(np.float32)
-    #
-    # target_shape = tf.constant(np.array([64, 64, 64]))
-    # dataset = dataset.map(lambda data: tuple(tf.py_func(_resize_py_function,
-    #                                                     [data, target_shape],
-    #                                                     [tf.float32])))
-    # sess = tf.Session()
+
     dataset = tf.data.Dataset.zip((dataset, dataset))
     dataset = dataset.cache(cache)
     if shuffle:
@@ -125,26 +62,74 @@ def _get_data(nthreads, batch_size, src_folder, n_epochs, cache, shuffle):
     dataset = dataset.repeat(n_epochs)
     return dataset, target_shape
 
+class Peaks2MapsDataset:
 
-from nilearn.plotting import plot_glass_brain
-import io
-def gen_plot(data):
-    args = {"colorbar": False,
-            "plot_abs": False,
-            "threshold": 0,
-            "cmap": "RdBu",
-            }
-    target_affine = np.array([[4., 0., 0., -75.],
-                              [0., 4., 0., -105.],
-                              [0., 0., 4., -70.],
-                              [0., 0., 0., 1.]])
-    nii = nb.Nifti1Image(np.squeeze(data), target_affine)
-    buf = io.BytesIO()
-    p = plot_glass_brain(nii, black_bg=False,
-                         display_mode='lyrz',
-                         **args)
-    p.savefig(buf)
-    p.close()
-    buf.seek(0)
-    val = buf.getvalue()
-    return val
+    def __init__(self, target_resolution, train_batch_size,
+                 test_batch_size, n_epochs, nthreads=None):
+        self.target_resolution = target_resolution
+        self.train_batch_size = train_batch_size
+        self.test_batch_size = test_batch_size
+        self.n_epochs = n_epochs
+        if nthreads is None:
+            import multiprocessing
+            self.nthreads = multiprocessing.cpu_count()
+        else:
+            self.nthreads = nthreads
+
+        self.training_dataset, self.target_shape = _get_data(self.nthreads,
+                                                                  self.train_batch_size,
+                                                                     "D:/data/hcp_statmaps/train",
+                                                                     100,
+                                                                     'D:/drive/workspace/peaks2maps/cache_train',
+                                                                     True,
+                                                                  self.target_resolution)
+        self.validation_dataset, validation_shape = _get_data(self.nthreads,
+                                                              self.test_batch_size,
+                                                              "D:/data/hcp_statmaps/val",
+                                                              1,
+                                                              'D:/drive/workspace/peaks2maps/cache_val',
+                                                              False,
+                                                              self.target_resolution)
+        assert(self.target_shape == validation_shape)
+
+        self.handle = tf.placeholder(tf.string, shape=[])
+        iterator = tf.data.Iterator.from_string_handle(
+            self.handle, self.training_dataset.output_types,
+            self.training_dataset.output_shapes)
+        self.input_image, self.target_image = iterator.get_next()
+
+        # You can use feedable iterators with a variety of different kinds of iterator
+        # (such as one-shot and initializable iterators).
+        self.training_iterator = self.training_dataset.make_one_shot_iterator()
+        self.validation_iterator = self.validation_dataset.make_initializable_iterator()
+
+
+    def get_plot_op(self, image, summary_label):
+        target_affine, _ = _get_resize_arg(self.target_resolution)
+        target_affine_tf = tf.constant(target_affine)
+
+        def _gen_plot(data, target_affine):
+            args = {"colorbar": False,
+                    "plot_abs": False,
+                    "threshold": 0,
+                    "cmap": "RdBu",
+                    }
+            nii = nb.Nifti1Image(np.squeeze(data), target_affine)
+            buf = io.BytesIO()
+            p = plot_glass_brain(nii, black_bg=False,
+                                 display_mode='lyrz',
+                                 **args)
+            p.savefig(buf)
+            p.close()
+            buf.seek(0)
+            val = buf.getvalue()
+            return val
+
+        plot = tf.py_func(_gen_plot, [image, target_affine_tf], tf.string)
+        image = tf.image.decode_png(plot)
+        # Add the batch dimension
+        image = tf.expand_dims(image, 0)
+        summary_image = tf.summary.image(summary_label,
+                                         image, max_outputs=200000)
+        return summary_image
+
--- pix2pix_from_scratch.py
+++ pix2pix_from_scratch.py
@@ -2,7 +2,7 @@ import time
 import os
 import tensorflow as tf
 import trivial_model as model
-from datasets import get_data, gen_plot
+from datasets import Peaks2MapsDataset
 import numpy as np
 
 batch_size = 10
@@ -11,67 +11,39 @@ log_dir = 'test_log'
 max_steps = 2000
 
 
-def do_eval(sess,
-            eval_correct,
-            images_placeholder,
-            labels_placeholder,
-            data_set):
-    """Runs one evaluation against the full epoch of data.
-    Args:
-      sess: The session in which the model has been trained.
-      eval_correct: The Tensor that returns the number of correct predictions.
-      images_placeholder: The images placeholder.
-      labels_placeholder: The labels placeholder.
-      data_set: The set of images and labels to evaluate, from
-        input_data.read_data_sets().
-    """
-    input_images_placeholder, target_images_placeholder, input_shape = get_data(
-        1)
-
-    inference_model = model.get_inference(input_images_placeholder,
-                                          input_shape)
-    eval = tf.contrib.metrics.streaming_pearson_correlation(inference_model,
-                                                            target_images_placeholder)
-    corrs = []
-    while True:
-        try:
-            feed_dict = data_set.fill_feed_dict(images_placeholder,
-                                                labels_placeholder)
-            corrs.append(sess.run(eval, feed_dict=feed_dict))
-        except tf.errors.OutOfRangeError:
-            break
-
-    print('  Num examples: %d  Num correct: %d  Precision @ 1: %0.04f' %
-          (num_examples, true_count, precision))
-
 def run_training():
     """Train MNIST for a number of steps."""
 
     # Tell TensorFlow that the model will be built into the default Graph.
     with tf.Graph().as_default():
+
+        ds = Peaks2MapsDataset(target_resolution=4.0,
+                               n_epochs=100,
+                               train_batch_size=20,
+                               test_batch_size=1)
         # Generate placeholders for the images and labels.
-        input_images, target_images, input_shape, handle, training_iterator, validation_iterator = get_data(batch_size)
+        #input_images, target_images, input_shape, handle, training_iterator, validation_iterator = get_data(batch_size)
 
         # Build a Graph that computes predictions from the inference model.
-        inference_model = model.get_inference(input_images,
-                                              input_shape)
+        inference_model = model.get_inference(ds.input_image,
+                                              ds.target_shape)
 
         # Add to the Graph the Ops for loss calculation.
-        loss = model.get_loss(inference_model, target_images)
+        loss = model.get_loss(inference_model, ds.target_image)
 
         # Add to the Graph the Ops that calculate and apply gradients.
         train_op = model.get_training(loss, learning_rate)
 
         eval = tf.contrib.metrics.streaming_pearson_correlation(
             inference_model,
-            target_images)
+            ds.target_image)
 
         # Build the summary Tensor based on the TF collection of Summaries.
         summary = tf.summary.merge_all()
 
         # Add the variable initializer Op.
         init = tf.group(tf.global_variables_initializer(),
-                 tf.local_variables_initializer())
+                        tf.local_variables_initializer())
 
         # Create a saver for writing training checkpoints.
         saver = tf.train.Saver()
@@ -83,7 +55,7 @@ def run_training():
         train_summary_writer = tf.summary.FileWriter(os.path.join(log_dir, "train"),
                                                                   sess.graph)
         test_summary_writer = tf.summary.FileWriter(os.path.join(log_dir, "test"),
-            sess.graph)
+                                                    sess.graph)
 
         mean_correlation_ = tf.placeholder(tf.float32, shape=())
         mean_correlation_summary = tf.summary.scalar('mean_correlation', mean_correlation_)
@@ -91,8 +63,8 @@ def run_training():
         correlation_histogram_summary = tf.summary.histogram('correlation_histogram',
                                                              correlations_)
 
-        summary_image_output = summary_glass_brain(inference_model, 'example_1_output')
-        summary_image_target = summary_glass_brain(target_images, 'example_1_target')
+        summary_image_output = ds.get_plot_op(inference_model, 'example_1_output')
+        summary_image_target = ds.get_plot_op(ds.target_image, 'example_1_target')
 
         # And then after everything is built:
 
@@ -101,8 +73,8 @@ def run_training():
 
         # The `Iterator.string_handle()` method returns a tensor that can be evaluated
         # and used to feed the `handle` placeholder.
-        training_handle = sess.run(training_iterator.string_handle())
-        validation_handle = sess.run(validation_iterator.string_handle())
+        training_handle = sess.run(ds.training_iterator.string_handle())
+        validation_handle = sess.run(ds.validation_iterator.string_handle())
 
         # Start the training loop.
         step = 0
@@ -116,7 +88,7 @@ def run_training():
                 # in the list passed to sess.run() and the value tensors will be
                 # returned in the tuple from the call.
                 _, loss_value = sess.run([train_op, loss],
-                                         feed_dict={handle: training_handle})
+                                         feed_dict={ds.handle: training_handle})
 
                 duration = time.time() - start_time
 
@@ -124,17 +96,17 @@ def run_training():
                 if step % 100 == 0:
                     print('Step %d: loss = %.2f (%.3f sec)' % (
                         step, loss_value, duration))
-                    summary_str = sess.run(summary, feed_dict={handle: training_handle})
+                    summary_str = sess.run(summary, feed_dict={ds.handle: training_handle})
                     train_summary_writer.add_summary(summary_str, step)
                     train_summary_writer.flush()
 
-                    sess.run(validation_iterator.initializer)
+                    sess.run(ds.validation_iterator.initializer)
                     if step == 0:
                         sum_img_trg = sess.run(summary_image_target,
                             feed_dict={
-                                handle: validation_handle})
+                                ds.handle: validation_handle})
                         test_summary_writer.add_summary(sum_img_trg, step)
-                        sess.run(validation_iterator.initializer)
+                        sess.run(ds.validation_iterator.initializer)
                     corrs = []
                     while True:
                         try:
@@ -142,21 +114,20 @@ def run_training():
                                 corr, sum_img = sess.run(
                                         [eval, summary_image_output],
                                         feed_dict={
-                                            handle: validation_handle})
+                                            ds.handle: validation_handle})
                             else:
                                 corr = sess.run(eval, feed_dict={
-                                    handle: validation_handle})
+                                    ds.handle: validation_handle})
                             corrs.append(corr[0])
                         except tf.errors.OutOfRangeError:
                             break
                     corrs = np.array(corrs)
 
                     sum = sess.run(mean_correlation_summary, feed_dict={mean_correlation_: np.nanmean(corrs),
-                                                                  handle: validation_handle})
+                                                                  ds.handle: validation_handle})
                     # sess.run(correlation_histogram_summary, feed_dict={correlations_: corrs,
                     #                                                    handle: validation_handle})
                     test_summary_writer.add_summary(sum, step)
-                    # Add image summary
                     test_summary_writer.add_summary(sum_img, step)
                     test_summary_writer.flush()
 
@@ -165,14 +136,7 @@ def run_training():
             step += 1
 
 
-def summary_glass_brain(inference_model, label):
-    plot = tf.py_func(gen_plot, [inference_model], tf.string)
-    image = tf.image.decode_png(plot)
-    # Add the batch dimension
-    image = tf.expand_dims(image, 0)
-    summary_image = tf.summary.image(label,
-                                     image, max_outputs=200000)
-    return summary_image
+
 
 
 if __name__ == '__main__':
